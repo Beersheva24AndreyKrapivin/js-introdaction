207a8b7e3de7945644035f34fb477376c036b8f2
Андрей Крапивин <without.a.body@gmail.com>
2025-01-20 11:10:23 +0200

    FromIntToString

--- conversion-functions.mjs
+++ conversion-functions.mjs
@@ -2,21 +2,21 @@
 export function myParseInt(strNum) {
     let res = NaN;
     let sign = 1;
-    if(strNum != null && strNum != undefined) {
+    if (strNum != null && strNum != undefined) {
         let index = 0;
-        
+
         strNum = strNum.toString();
         strNum = strNum.trim();
-        if(strNum[0] == '-'){
+        if (strNum[0] == '-') {
             index++;
             sign = -1;
-        } else if(strNum[0] == '+') {
+        } else if (strNum[0] == '+') {
             index++;
         }
-        if(index < strNum.length && !isNaN(getDigit(strNum[index]))) {
+        if (index < strNum.length && !isNaN(getDigit(strNum[index]))) {
             res = 0;
             let running = true;
-            while(index < strNum.length && running) {
+            while (index < strNum.length && running) {
                 let digit = getDigit(strNum[index]);
                 if (isNaN(digit)) {
                     running = false;
@@ -24,15 +24,50 @@ export function myParseInt(strNum) {
                     res = res * 10 + digit;
                     index++;
                 }
-                
+
             }
-           
+
         }
 
     }
-    return res *= sign;
+    return res * sign;
 }
 function getDigit(digitStr) {
     let res = digitStr >= '0' && digitStr <= '9' ? +digitStr : NaN;
     return res;
+}
+
+export function myToStringFromIntNumber(number) {
+    //TODO returns sting presentation of the given number
+    //if number has type of string the string should contain 
+    //      a number matching the parsInt syntax
+    // examples:
+    //myToStringFromIntNumber(12.35) -> returns "12"
+    //myToStringFromIntNumber("12.35") -> returns "12"
+    //myToStringFromIntNumber() -> returns ""
+    //myToStringFromIntNumber(-12) -> returns "-12"
+    //myToStringFromIntNumber("+12") -> returns "12"
+    //myToStringFromIntNumber(+12) -> returns "12"
+    //myToStringFromIntNumber("a1") -> returns ""
+    //myToStringFromIntNumber("1a") -> returns "1"
+    //Disallowed the following operations:
+    //toString() using
+    //constructor String
+    //operator + with empty string like "" + 
+    let res = "";
+    let sign = "";
+    const temp = "0123456789";
+    let numPars = myParseInt(number);
+    if (!isNaN(numPars)) {
+        if (numPars < 0) {
+            sign = "-";
+            numPars = -numPars;
+        }
+        while (numPars > 0) {
+            let digit = numPars % 10;
+            numPars = Math.floor(numPars / 10);
+            res = temp[digit] + res;
+        }
+    }
+    return sign + res;
 }
--- test/conversion-functions.test.mjs
+++ test/conversion-functions.test.mjs
@@ -1,40 +1,79 @@
-import {describe, it, expect} from 'vitest';
-import { myParseInt } from '../conversion-functions.mjs';
+import { describe, it, expect, test } from 'vitest';
+import { myParseInt, myToStringFromIntNumber } from '../conversion-functions.mjs';
 //Unit test is AAA - Arranging / Act / Assertion
 describe("myParseInt test suit", () => {
-   it("reqular string with positive integer number", () => {
+    it("reqular string with positive integer number", () => {
         const strNum = "12"; //Arranging
         const res = myParseInt(strNum) + 2; //Act
         expect(res).toBe(14); //Assertion
-   }) ;
-   it ("reqular string with negative integer number", () => {
-    expect(myParseInt("-12")).toBe(-12);
-   });
-   it ("string with  number following +", () => {
-    expect(myParseInt("+12")).toBe(12)
-   });
-   it ("undefined", () => {
-    expect(myParseInt()).toBeNaN();
-   });
-   it ("null", () => {
-    expect(myParseInt(null)).toBeNaN();
-   });
-   it ("float number inside a string", () => {
-    expect(myParseInt("12.35")).toBe(12);
-   });
-   it("string beginning with the space", () => {
-    expect(myParseInt(" 12")).toBe(12);
-   }),
-   it ("space in middle", () => {
-    expect(myParseInt("12 35")).toBe(12);
-   });
-   it ("first symbol is not a number", () => {
-    expect(myParseInt("a1")).toBeNaN()
-   });
-   it ("string begins from ++", () => {
-    expect(myParseInt("++12")).toBeNaN()
-   });
-   it ("space following -", () => {
-    expect(myParseInt("- 12")).toBeNaN();
-   })
+    });
+    it("reqular string with negative integer number", () => {
+        expect(myParseInt("-12")).toBe(-12);
+    });
+    it("string with  number following +", () => {
+        expect(myParseInt("+12")).toBe(12)
+    });
+    it("undefined", () => {
+        expect(myParseInt()).toBeNaN();
+    });
+    it("null", () => {
+        expect(myParseInt(null)).toBeNaN();
+    });
+    it("float number inside a string", () => {
+        expect(myParseInt("12.35")).toBe(12);
+    });
+    it("string beginning with the space", () => {
+        expect(myParseInt(" 12")).toBe(12);
+    }),
+        it("space in middle", () => {
+            expect(myParseInt("12 35")).toBe(12);
+        });
+    it("first symbol is not a number", () => {
+        expect(myParseInt("a1")).toBeNaN()
+    });
+    it("string begins from ++", () => {
+        expect(myParseInt("++12")).toBeNaN()
+    });
+    it("space following -", () => {
+        expect(myParseInt("- 12")).toBeNaN();
+    })
+})
+
+describe("myToStringFromIntNumber test suit", () => {
+    it("reqular string with negative integer number", () => {
+        expect(myToStringFromIntNumber(12.35)).toBe("12");
+    });
+    it("reqular string with negative integer number", () => {
+        expect(myToStringFromIntNumber("12.35")).toBe("12");
+    });
+    it("string with  number following +", () => {
+        expect(myToStringFromIntNumber()).toBe("")
+    });
+    it("undefined", () => {
+        expect(myToStringFromIntNumber(-12)).toBe("-12");
+    });
+    it("undefined", () => {
+        expect(myToStringFromIntNumber("-12")).toBe("-12");
+    });
+    it("null", () => {
+        expect(myToStringFromIntNumber("+12")).toBe("12");
+    });
+    it("float number inside a string", () => {
+        expect(myToStringFromIntNumber(+12)).toBe("12");
+    });
+    it("float number inside a string", () => {
+        expect(myToStringFromIntNumber("a1")).toBe("");
+    });
+    it("float number inside a string", () => {
+        expect(myToStringFromIntNumber("1a")).toBe("1");
+    });
+})
+
+describe ("equal operators", () => {
+    it("simple equality operator ==", () => {
+        expect(12 == '12').toBeTruthy();
+    })
+    it("strong equlity operator ===", () => {
+        expect(12 === '12').toBeFalsy();
+    })
 })
