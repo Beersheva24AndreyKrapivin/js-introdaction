43f658c710d98db823246e4e0738b03abceefa9b
Андрей Крапивин <without.a.body@gmail.com>
2025-01-22 14:08:34 +0200

    new branch functions

---
+++ functions.mjs
@@ -0,0 +1,16 @@
+// function sum(num1, num2) {
+//     return this.x * num1 + this.x * num2;
+// }
+// sum.a = 2;
+// sum(10, 10);
+// sum.call({x:5}, 10, 10);
+
+const point = {
+    x: 5,
+    y: 10
+};
+function displayPoint(z) {
+    console.log(`x:${this.x}; y:${this.y}; z:${z}`);
+}
+const display = displayPoint.bind(point, 20);
+display();
