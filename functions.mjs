bf70e46e4423f391c5a60d34bf414489b55fc1f8
Андрей Крапивин <without.a.body@gmail.com>
2025-01-22 15:41:04 +0200

    CW. bind, call, apply - difference b/w reg function and arrow function

--- functions.mjs
+++ functions.mjs
@@ -9,8 +9,17 @@ const point = {
     x: 5,
     y: 10
 };
-function displayPoint(z) {
+function displayPoint(z, d, f, g) {
+    console.log(`x:${this.x}; y:${this.y}; z:${arguments[0]}`);
+}
+const display = displayPoint.bind(point);
+display(40, "abc", 30, true);
+
+const displayPointArrow = (z) => {
     console.log(`x:${this.x}; y:${this.y}; z:${z}`);
 }
-const display = displayPoint.bind(point, 20);
-display();
+// Arrow function doesn't have its own this
+// const displayArrow = displayPointArrow.bind(point);
+// displayArrow(40);
+//display.call(point, 40, 30, true, false);
+// display.apply(point, [40, 30, true, false]);
