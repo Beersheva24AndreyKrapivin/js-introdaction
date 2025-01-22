e9deea01c6c1ed2d16590ea242a6967720287dfb
Андрей Крапивин <without.a.body@gmail.com>
2025-01-22 12:41:34 +0200

    objects, keys, vealues, toString

--- objects.mjs
+++ objects.mjs
@@ -16,18 +16,33 @@ export function getOccurencesObject(string) {
     //"abcadab"
     //data should include a -> encountered 3 times
     //b -> two times, c and d -> one time
-    const occurences = {};
+
+    // const occurences = {};
     
-    if (string) {
-        const actualString = string.toString();
-        for (const char of actualString) {
-            if (char in occurences) {
-                occurences[char]++;
-            } else {
-                occurences[char] = 1;
+    // if (string != undefined && string != null) {
+    //     const actualString = string.toString();
+    //     for (const char of actualString) {
+    //         if (occurences[char]) {
+    //             occurences[char]++;
+    //         } else {
+    //             occurences[char] = 1;
+    //         }
+    //     }
+    // }
+    
+    // return occurences;
+
+    const res = {};
+    if (string != undefined && string != null) {
+        string = string.toString();
+        for (let i = 0; i < string.length; i++) {
+            let char = string[i];
+            if (!res[char]) {
+                res[char] = 0;
             }
+            res[char]++;
         }
     }
+    return res;
     
-    return occurences;
 }
