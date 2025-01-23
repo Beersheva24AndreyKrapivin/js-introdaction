34e532500e0599e226ad6b9fa1a9d7cecd9965f2
Андрей Крапивин <without.a.body@gmail.com>
2025-01-23 12:25:54 +0200

    HW myBind

--- functions.mjs
+++ functions.mjs
@@ -5,21 +5,32 @@
 // sum(10, 10);
 // sum.call({x:5}, 10, 10);
 
-const point = {
-    x: 5,
-    y: 10
-};
-function displayPoint(z, d, f, g) {
-    console.log(`x:${this.x}; y:${this.y}; z:${arguments[0]}`);
-}
-const display = displayPoint.bind(point);
-display(40, "abc", 30, true);
+// const point = {
+//     x: 5,
+//     y: 10
+// };
+// function displayPoint(z, d, f, g) {
+//     console.log(`x:${this.x}; y:${this.y}; z:${arguments[0]}`);
+// }
+// const display = displayPoint.bind(point);
+// display(40, "abc", 30, true);
 
-const displayPointArrow = (z) => {
-    console.log(`x:${this.x}; y:${this.y}; z:${z}`);
-}
+// const displayPointArrow = (z) => {
+//     console.log(`x:${this.x}; y:${this.y}; z:${z}`);
+// }
+//
 // Arrow function doesn't have its own this
 // const displayArrow = displayPointArrow.bind(point);
 // displayArrow(40);
 //display.call(point, 40, 30, true, false);
 // display.apply(point, [40, 30, true, false]);
+
+export function myBind(thisArg) {
+    //apply method "apply" 
+    const oldThis = this;
+
+    return function() {
+        return oldThis.apply(thisArg, arguments);
+    }; 
+}
+
